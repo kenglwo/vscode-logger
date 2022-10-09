55f7f772f743283e9ca3cfbba57c9303cbd7fd6f
hiroka <hiroka@hirokanoMacBook-Air.local>
2022-10-09 17:09:53 +0900

    rename directory and publish package

---
+++ js-logger/README.md
@@ -0,0 +1,13 @@
+# vscode-logger
+
+This extension logs the JavaScript files you write.
+This extension was created for the Kobe University course "Basic Programming Exercise I".
+
+## Usage
+
+1. Install this extension.
+2. Open command palette (Windows: control + shift + p, Mac: command + shift + p).
+3. Enter "log" in the command palette and execute the log command.
+4. Enter the student ID number in the input window that appears in the vscode.
+5. This completes the setup. Each time you save a JavaScript file, the code is saved to the database.
+
---
+++ js-logger/src/extension.ts
@@ -0,0 +1,92 @@
+import * as vscode from 'vscode';
+import { parseScript } from 'esprima';
+import { ted } from 'edit-distance';
+import fetch from 'node-fetch';
+import { config } from 'dotenv';
+config({ path: __dirname + '/../.env' });
+
+
+const API_ENDPOINT: any = process.env.API_ENDPOINT;
+const API_KEY: any = process.env.API_KEY;
+
+async function insertOne(id: string, savedAt: string, code: string, sloc: number, ted: number) {
+  const options = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Request-Headers': '*',
+          'api-key': API_KEY
+      },
+      body: JSON.stringify({
+          'collection': 'codeparams',
+          'database': 'test',
+          'dataSource': 'Cluster0',
+          'document': {
+            'id': id,
+            'savedAt': savedAt,
+            'code': code,
+            'sloc': sloc,
+            'ted': ted
+          }
+        })
+      };
+
+  const res = await fetch(API_ENDPOINT, options);
+  const resJson = await res.json();
+  return resJson.insertedId;
+}
+
+
+function calcTed(lastSourceCode: string, currentSourceCode: string): number {
+  const lastAst = parseScript(lastSourceCode);
+  const currentAst = parseScript(currentSourceCode);
+
+  let insert = function(node: any) { return 1; };
+  let remove = insert;
+  let update = function(nodeA: any, nodeB: any) { 
+    return nodeA.body !== nodeB.body ? 1 : 0;
+  };
+  let children = function(node: any) { return node.body; };
+
+  let astEditDistance: number = 0;
+  try {
+    astEditDistance = ted(lastAst, currentAst, children, insert, remove, update).distance;
+  } catch (e) {
+    console.log(e);
+  }
+  return astEditDistance;
+}
+
+
+export async function activate(context: vscode.ExtensionContext) {
+  vscode.window.showInformationMessage('extension enabled');
+
+  const disposable = vscode.commands.registerCommand('js-logger.start', async () => {
+    vscode.window.showInformationMessage('logger start');
+    const studentId: any = await vscode.window.showInputBox();
+
+    let lastSourceCode: string = ''; 
+
+    vscode.workspace.onDidSaveTextDocument(async (document: vscode.TextDocument) => {
+      if (document.languageId !== 'javascript') return;
+      const currentDate: string = new Date().toLocaleString(); 
+      const sourceCode: string = document.getText();
+      const sloc: number = sourceCode.split('\n').length;
+      const ted: number = calcTed(lastSourceCode, sourceCode);
+
+      try {
+        const res = await insertOne(studentId, currentDate, sourceCode, sloc, ted);
+        vscode.window.showInformationMessage(`savedId: ${res}`);
+      } catch (e: any) {
+        vscode.window.showInformationMessage(e.message);
+      }
+
+      lastSourceCode = sourceCode;
+    });
+
+  });
+  context.subscriptions.push(disposable);
+}
+
+
+export function deactivate() {};
